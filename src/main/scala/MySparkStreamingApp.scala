9ed39e7c900979664ef7b478eaa8bde90882e4d5
xiaoy <yu.xiao@seaboxdata.com>
2017-11-24 18:08:36 +0800

    Almost finished the first POC test.

--- src/main/scala/MySparkStreamingApp.scala
+++ src/main/scala/MySparkStreamingApp.scala
@@ -10,68 +10,35 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.slf4j.LoggerFactory
+import org.apache.spark.streaming._
 
 /**
   * Created by xiaoy on 2017/11/22.
   */
 object MySparkStreamingApp {
   def main(args: Array[String]): Unit = {
-    //    val logger = LoggerFactory.getLogger(MySparkStreamingApp.getClass)
-    //
-    //    Logger.getLogger("org.apache.kafka").setLevel(Level.WARN)
-    //    Logger.getLogger("org.apache.zookeeper").setLevel(Level.WARN)
-    //    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    //    Logger.getLogger("kafka.utils").setLevel(Level.WARN)
-    //
-    //    val conf = new SparkConf()
-    //      .setMaster("local[10]")
-    //      .setAppName("MySparkStreamingApp")
-    //      .set("spark.streaming.kafka.maxRatePerPartition", "250000")
-    //    val ssc = new StreamingContext(conf, Seconds(5))
 
-    //    val lines = ssc.socketTextStream("localhost", 5678)
-    //
-    //    val words = lines.flatMap(_.split(" "))
-    //
-    //    val pairs = words.map(word => (word, 1))
-    //    val wordCounts = pairs.reduceByKey(_ + _)
-    //
-    //    wordCounts.print()
-
-    //    val topics = "test7"
-    //    val brokers = "host1:6667,host2:6667,host3:6667,host4:6667,host5:6667"
-    //
-    //    val topicsSet = topics.split(",").toSet
-    //    val kafkaParams = Map[String, String](
-    //      "metadata.broker.list" -> brokers,
-    //      "group.id" -> "group_of_xiaoy",
-    //      "auto.offset.reset" -> "smallest"
-    //    )
-    //
-    //    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-    //      ssc, kafkaParams, topicsSet)
-
-    //    val offsetList = List((topics, 0, 22753623L),(topics, 1, 327041L))                          //指定topic，partition_no，offset
-    //    val fromOffsets = setFromOffsets(offsetList)     //构建参数
-    //
-    //    val messageHandler = (mam: MessageAndMetadata[String, String]) => (mam.topic, mam.message()) //构建MessageAndMetadata
-    //
-    //    val messages: InputDStream[(String, String)]  =
-    //      KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)]
-    //    (ssc, kafkaParams, fromOffsets, messageHandler)
-
-
-    //    val lines = messages.map(_._2)
-
-
-    val context = StreamingContext.getOrCreate("/user/root/xiaoy/cp",
-      MySparkStreamingApp.functionToCreateContext)
+    val context =
+      if (args(0).equals("local")) {
+        createStreamingContext()
+      } else {
+        StreamingContext.getOrCreate("/user/root/xiaoy/cp",
+          MySparkStreamingApp.functionToCreateContext)
+      }
 
     context.start() // Start the computation
     context.awaitTermination() // Wait for the computation to terminate
   }
 
   def functionToCreateContext(): StreamingContext = {
+
+    val ssc = createStreamingContext()
+    ssc.checkpoint("/user/root/xiaoy/cp") // set checkpoint directory
+
+    ssc
+  }
+
+  def createStreamingContext(): StreamingContext = {
     val logger = LoggerFactory.getLogger(MySparkStreamingApp.getClass)
 
     Logger.getLogger("org.apache.kafka").setLevel(Level.WARN)
@@ -82,10 +49,9 @@ object MySparkStreamingApp {
     val conf = new SparkConf()
       .setMaster("local[10]")
       .setAppName("MySparkStreamingApp")
-      .set("spark.streaming.kafka.maxRatePerPartition", "1")
-    val ssc = new StreamingContext(conf, Seconds(5))
-
-    val topics = "test7"
+      .set("spark.streaming.kafka.maxRatePerPartition", "100")
+    val ssc = new StreamingContext(conf, Seconds(3))
+    val topics = "sztran"
     val brokers = "host1:6667,host2:6667,host3:6667,host4:6667,host5:6667"
 
     val topicsSet = topics.split(",").toSet
@@ -98,19 +64,39 @@ object MySparkStreamingApp {
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topicsSet)
 
-    val lines = messages.map(m => m._2.split("\\^\\|")(0) -> m._2)
-//    val lines = messages.map(m => m._2)
+    val recordDStream = messages.map(m => {
+      val fields = m._2.split("\\|")
+      val now = new Date().getTime
+      fields(0) -> (now, fields(7).toFloat, fields(7).toFloat)
+    })
 
-    lines.print(2)
+    val record3mWindow =
+      recordDStream.reduceByKeyAndWindow((x: (Long, Float, Float), y: (Long, Float, Float)) => {
+        (Math.min(x._1, y._1), Math.max(x._2, y._2), Math.min(x._3, y._3))
+      }, Seconds(30), Seconds(3))
+        .filter(x => {
+          val v = x._2
+          (v._2 - v._3)/v._3 > 0.22
+        })
 
-    lines.foreachRDD((rdd, t) => {
-      val d = new Date(t.milliseconds)
-      println("At time %s received %d messages"
-        .format(new SimpleDateFormat("hh:mm:ss SSS").format(new Date(t.milliseconds)), rdd.count()))
-    }
-    )
+    record3mWindow.foreachRDD(rdd => {
+      val now = new Date()
+      val format = new SimpleDateFormat("hh:mm:ss.SSS")
 
-    ssc.checkpoint("/user/root/xiaoy/cp") // set checkpoint directory
+      rdd.collect().foreach(x => {
+        println("[%s]: %s  %-10.2f %-10.2f %s".format(
+          x._1, format.format(now), x._2._2, x._2._3, format.format(new Date(x._2._1))))
+      })
+    })
+
+    //    record3mWindow.print(2)
+    //
+    //    recordDStream.foreachRDD((rdd, t) => {
+    //      val d = new Date(t.milliseconds)
+    //      println("At time %s received %d messages"
+    //        .format(new SimpleDateFormat("hh:mm:ss SSS").format(new Date(t.milliseconds)), rdd.count()))
+    //    }
+    //    )
 
     ssc
   }
